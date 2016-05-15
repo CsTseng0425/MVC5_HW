5edbb4fe170bb3bce07f6b241cbdbb4b3dcbf987
CsTseng0425 <melancholy_0425@yahoo.com.tw>
2016-05-15 19:26:14 +0800

    使用ViewModel

---
+++ MVC2/Controllers/ProductsVMController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC2.ViewModel;
+using MVC2.Repositories;
+using MVC2.Service;
+
+namespace MVC2.Controllers
+{
+    public class ProductsVMController : Controller
+    {
+      
+        private readonly ProductVMService _productSvc;
+
+        public ProductsVMController()
+        {
+            var unitOfWork = new EFUnitOfWork();
+            _productSvc = new ProductVMService(unitOfWork);
+
+        }
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View(_productSvc.Lookup());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var product = _productSvc.Edit(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] ProductsViewModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductId = _productSvc.GetMaxID() + 1;
+                _productSvc.Add(product);
+                _productSvc.Save();
+                //  UnitOfWork.Save(); => 拉出出執行,比較明確
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = _productSvc.Edit(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Orders/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] ProductsViewModel product)
+        {
+            if (_productSvc.Edit(product))
+            {
+                _productSvc.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = _productSvc.Edit(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var product = _productSvc.GetSingle(id);
+            _productSvc.Delete(product);
+            _productSvc.Save();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVC2/Models/ProductVMService.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC2.Models;
+using MVC2.Repositories;
+using MVC2.ViewModel;
+
+namespace MVC2.Service
+{
+    public class ProductVMService : Repository<Product>
+    {
+        private readonly IRepository<Product> _productRep;
+
+     
+        public ProductVMService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _productRep = new Repository<Product>(unitOfWork);
+            
+        }
+
+        public IEnumerable<ProductsViewModel> Lookup()
+        {
+            var source = _productRep.LookupAll().Take(5);
+            var result = source.Select(product => new ProductsViewModel()
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                Active = product.Active,
+                Price = product.Price,
+                Stock = product.Stock,
+              
+            }).ToList();
+            return result;
+        }
+
+        public ProductsViewModel GetSingle(int PId)
+        {
+            //  return _productRep.GetSingle(d => d.ProductId == PId);
+
+            var source = _productRep.GetSingle(d => d.ProductId == PId);
+            
+            ProductsViewModel product = new ProductsViewModel 
+            {
+                ProductId = source.ProductId,
+                ProductName = source.ProductName,
+                Active = source.Active,
+                Price = source.Price,
+                Stock = source.Stock,
+
+            };
+            return product;
+        }
+
+        public int GetMaxID()
+        {
+            return _productRep.LookupAll().Max(d => d.ProductId);
+
+        }
+
+        public void Add(ProductsViewModel product)
+        {
+            var result = new Product()
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                Price = product.Price,
+                Active = product.Active,
+                Stock = product.Stock
+            };
+
+            Add(result);
+        }
+
+        public void Add(Product product)
+        {
+            _productRep.Create(product);
+           
+          
+        }
+
+        public ProductsViewModel Edit(int pid)
+        {
+            var hasOldData = _productRep.Query(d => d.ProductId == pid).Count() > 0;
+            Product product = _productRep.GetSingle(d => d.ProductId == pid);
+            if (hasOldData)
+            {
+                ProductsViewModel VMproduct
+                = new ProductsViewModel
+                {
+                    ProductId = product.ProductId,
+                    ProductName = product.ProductName,
+                    Price = product.Price,
+                    Active = product.Active,
+                    Stock = product.Stock
+                };
+                return VMproduct;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public bool Edit(ProductsViewModel pageData)
+        {
+            var hasOldData = _productRep.Query(d => d.ProductId == pageData.ProductId).Count() > 0;
+         
+            if(hasOldData )
+            {
+                Product oldProduct = _productRep.GetSingle(d =>d.ProductId== pageData.ProductId);
+               oldProduct.ProductName = pageData.ProductName;
+                oldProduct.Price = pageData.Price;
+                oldProduct.Active = pageData.Active;
+                oldProduct.Stock = pageData.Stock;
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public bool Delete(int? pid)
+        {
+         
+            Product product = _productRep.GetSingle(d => d.ProductId == pid);
+            if (product==null)
+            {
+                return false;
+            }
+            else
+            {
+                Delete(product);
+                return true;
+            }
+
+        }
+
+
+
+
+        public bool Delete(ProductsViewModel data)
+        {
+            var hasData = _productRep.Query(d => d.ProductId==data.ProductId).Count()>0;
+
+            if(hasData)
+            {
+                Product delData = _productRep.GetSingle(d => d.ProductId == data.ProductId);
+                Delete(delData);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+
+        public void Delete(Product data)
+        {
+            _productRep.Remove(data);
+        }
+
+        public void Save()
+        {
+            _productRep.Commit();
+        }
+    }
+}
